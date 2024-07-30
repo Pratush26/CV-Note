4bb19c940b6f3567995cfd7bdc02e3feb859af9d
root <root@localhost>
2024-07-30 21:53:07 +0600

    auto publish 1722354783626

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-// Add all your JS here
+// Add all your JS her
 gsap.from(".wr",{
   y:30,
   duration:2,
   opacity:0,
   stagger:1
+})
+gsap.from("#sub",{
+  x:-50,
+  opacity:0,
+  scrollTrigger:{
+    trigger:"#sub",
+    scroller:"body",
+    start:"top 95%",
+    end:"top 100%",
+    scrub:2
+  }
 })
